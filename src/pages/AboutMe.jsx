f3aebb2e59e6d6bc27f3d13c9ea39fe4731b12dd
haohao1029 <ganjinghao2000@gmail.com>
2023-04-10 20:53:48 +0800

    :lipstick: (working on about me page)

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,27 @@
+
+
+export function AboutMe() {
+    return (
+        <>
+            <div className="container" id="content">
+                <div className="row ">
+                    <div className="glass">
+                        <h1>I am Kingsley</h1>
+                        <div className="col-6">
+                            asd
+                        </div>
+                        <div className="col-6">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                            voluptatum, quod, quia, voluptas quae voluptatem quibusdam
+                            necessitatibus voluptates quos quas nesciunt. Quisquam, quae
+                            voluptates. Quisquam, quae voluptates. Quisquam, quae voluptates.
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
