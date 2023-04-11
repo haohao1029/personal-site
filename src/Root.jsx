09823752aaa4900b9fe41e471dbb447f4e8af7bc
haohao1029 <ganjinghao2000@gmail.com>
2023-04-11 12:52:20 +0800

    :lipstick: (completed ABOUE ME page)

--- src/Root.jsx
+++ src/Root.jsx
@@ -8,7 +8,7 @@ export function Root() {
                 <h1 className="logo">Kingsley Gan</h1>
                 <nav>
                     <NavLink to="/">Home</NavLink>
-                    <NavLink to="/about-me">About Us</NavLink>
+                    <NavLink to="/about-me">About Me</NavLink>
                 </nav>
             </header>
             <Outlet />
